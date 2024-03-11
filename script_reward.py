5a7bd7b12ec2303d47fc7502117edb65e2550686
Lucas Carlos Giacomin <lucascgiacomin16@outlook.com>
2024-03-10 23:21:12 -0300

    esquece stop por agora

--- script_reward.py
+++ script_reward.py
@@ -6,13 +6,6 @@
 import pyautogui as pa
 import time
 import random
-from pynput import keyboard
-
-
-def wanna_stop():
-    if (keyboard.Key.esc):
-        return  True
-
 
 letras = ['q','w','e','r','t','y','u','i','o','p','a','s','d','f','g','h','j','k','l','z','x','c','v','b','n','m']
 
@@ -26,13 +19,9 @@ for i in letras:
     pa.press(i)
     time.sleep(0.5)
 
-    stop = wanna_stop()
-
     arrows_down = random.randint(1, 7)
     for i in range(arrows_down):
         pa.press('down')
-
-    stop = wanna_stop()
 
     time.sleep(1)
     pa.press('enter')
@@ -41,8 +30,6 @@ for i in letras:
 
     time.sleep(7)
 
-    stop = wanna_stop()
-
     pa.click(x = barra_pesquisa.x, y = barra_pesquisa.y)
 
     apagar= pa.locateCenterOnScreen('imagens/apagar.png', confidence= 0.9)
@@ -50,10 +37,4 @@ for i in letras:
 
     pa.click(x = apagar.x + 150, y = apagar.y)
 
-    stop = wanna_stop()
-
     time.sleep(2)
-
-    if stop:#recebe o resultado da pergunta feita em um dos pontos acima
-        print('Parado pelo teclado')
-        break
