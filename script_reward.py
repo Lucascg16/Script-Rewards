daf4eb6277c9ad2ee8fefbc352a92211925298ab
Lucas Carlos Giacomin <lucascgiacomin16@outlook.com>
2024-03-24 21:48:43 -0300

    Adicionando pesquisas para garantir os 90 pontos

--- script_reward.py
+++ script_reward.py
@@ -7,7 +7,7 @@ import pyautogui as pa
 import time
 import random
 
-letras = ['q','w','e','r','t','y','u','i','o','p','a','s','d','f','g','h','j','k','l','z','x','c','v','b','n','m']
+letras = ['q','w','e','r','t','y','u','i','o','p','a','s','d','f','g','h','j','k','l','z','x','c','v','b','n','m','1','2','3','4','5']
 
 time.sleep(1)
 
